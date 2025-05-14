18d8d18ef7e3b3c43b5c4c38cd7508b73e838db5
samedov.l <samedov.leon@outlook.com>
2025-05-14 12:39:40 +0300

    Задание 2 в рамках ДЗ 11. При вводе последовательности чисел также в конце вводим нечисло, чтобы завершить ввод последователности.

--- Homework-11/file1.cpp
+++ Homework-11/file1.cpp
@@ -1,9 +1,54 @@
-//Task 1
+////Task 1
+//
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <numeric>
+//#include <limits>
+//
+//int main() 
+//{
+//    std::vector<int> numbers;
+//    int x;
+//
+//    std::cout << "Please, enter a seqeunce of integers (enter a non-digit to complete):\n";
+//    while (std::cin >> x) 
+//    {
+//        numbers.push_back(x);
+//    }
+//
+//    std::cin.clear();
+//    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+//
+//    if (numbers.size() < 2) 
+//    {
+//        std::cerr << "Error! Not enough elements.\n";
+//        return 1;
+//    }
+//
+//    auto min_it = std::min_element(numbers.begin(), numbers.end());
+//    auto max_it = std::max_element(numbers.begin(), numbers.end());
+//
+//    // Defining the range boundaries between min and max, excluding the elements themselves
+//    auto begin = std::min(min_it, max_it) + 1;
+//    auto end = std::max(min_it, max_it);
+//
+//    int sum = std::accumulate(begin, end, 0);
+//
+//    if (max_it > min_it) 
+//    {
+//        std::cout << "The max element is located after the min one.\n";
+//    }
+//
+//    std::cout << "The sum between the max element amd the min one is: " << sum << "\n";
+//
+//    return 0;
+//}
 
+//Task 2
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
 #include <limits>
 
 int main() 
@@ -11,7 +56,7 @@ int main()
     std::vector<int> numbers;
     int x;
 
-    std::cout << "Please, enter a seqeunce of integers (enter a non-digit to complete):\n";
+    std::cout << "Please, enter a sequence of integers (enter a non-digit to complete):\n";
     while (std::cin >> x) 
     {
         numbers.push_back(x);
@@ -20,28 +65,30 @@ int main()
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    if (numbers.size() < 2) 
+    int k, val;
+    std::cout << "Please, input k (insertion step): ";
+    std::cin >> k;
+    std::cout << "Please, enter a value for insertion: ";
+    std::cin >> val;
+
+    if (k <= 0) 
     {
-        std::cerr << "Error! Not enough elements.\n";
+        std::cerr << "Error! k must be more than 0.\n";
         return 1;
     }
 
-    auto min_it = std::min_element(numbers.begin(), numbers.end());
-    auto max_it = std::max_element(numbers.begin(), numbers.end());
-
-    // Defining the range boundaries between min and max, excluding the elements themselves
-    auto begin = std::min(min_it, max_it) + 1;
-    auto end = std::max(min_it, max_it);
-
-    int sum = std::accumulate(begin, end, 0);
-
-    if (max_it > min_it) 
+    // Insert after every k-th element from the end to avoid index confusion
+    for (size_t i = k; i <= numbers.size(); i += k + 1) 
     {
-        std::cout << "The max element is located after the min one.\n";
+        numbers.insert(numbers.begin() + i, val);
     }
 
-    std::cout << "The sum between the max element amd the min one is: " << sum << "\n";
+    std::cout << "The result is:\n";
+    std::for_each(numbers.begin(), numbers.end(), [](int n) 
+        {
+        std::cout << n << " ";
+        });
+    std::cout << "\n";
 
     return 0;
 }
-
